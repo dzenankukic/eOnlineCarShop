1314c28cdd094ef2b59e1ef4612dd720474287ed
Haris-Basic <harisharebasic@gmail.com>
2021-01-11 03:24:57 +0100

    Dodavanje konekcijskog stringa

---
+++ eOnlineCarShop/Data/Migrations/20210111022406_Incijalna.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eOnlineCarShop.Data.Migrations
+{
+    public partial class Incijalna : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
