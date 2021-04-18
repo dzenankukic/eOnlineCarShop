a41f75784fe313eaefebefb2500f3056df1b2621
Haris Basic <basic.haris@edu.fit.ba>
2021-04-19 01:38:13 +0200

    Metoda GetUsers za Angular

--- eOnlineCarShop/Controllers/EmployeeController.cs
+++ eOnlineCarShop/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data_CS.Data;
+using eOnlineCarShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,41 @@ namespace eOnlineCarShop.Controllers
 {
     public class EmployeeController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public EmployeeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult GetUsers() // metoda koja vraca podatke za Angular
+        {
+            var model = _db.User
+                .Select(s => new ShowUsersVM {
+                userid = s.Id,
+                firstname = s.FirstName,
+                lastname = s.LastName,
+                birthdate = s.BirthDate.ToString(),
+                username = s.UserName,
+                email = s.Email,
+                emailconfirmed = s.EmailConfirmed,
+                phonenumber = s.PhoneNumber,
+                phonenumberconfirmed = s.PhoneNumberConfirmed
+            }).ToList();
+
+            return Json(model);
+        }
+
+        /*
+         Implementirati sl funkcije
+         
+        Update, Delete
+
+         */
     }
 }
---
+++ eOnlineCarShop/ViewModels/ShowUsersVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eOnlineCarShop.ViewModels
+{
+    public class ShowUsersVM
+    {
+        public int userid { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string birthdate { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public bool emailconfirmed { get; set; }
+        public string phonenumber { get; set; }
+        public bool phonenumberconfirmed { get; set; }
+    }
+}
