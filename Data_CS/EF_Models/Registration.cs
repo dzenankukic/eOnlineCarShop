67ce0f6bd6cb9d122a781432eb6fffaee44ce42c
Dženan Kukić <dzenan.kukic@edu.fit.ba>
2021-04-28 00:07:40 +0200

    Angular origin error handeler (helper-extensions)

---
+++ Data_CS/EF_Models/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_CS.EF_Models
+{
+    class Registration
+    {
+        //public int ID { get; set; }
+        //public int PhoneNumber { get; set; }
+        //public int Postcode { get; set; }
+        //public string HouseNumberName { get; set; }
+        //public string Email { get; set; }
+        //public string FirstName { get; set; }
+        //public string SurName { get; set; }
+        //public string Country { get; set; }
+
+
+    }
+}
