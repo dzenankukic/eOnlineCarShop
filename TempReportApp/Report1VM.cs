4b52ff3b52a9800d84e5c3a62047139971ef5960
Haris Basic <basic.haris@edu.fit.ba>
2021-08-06 19:08:13 +0200

    report za vozila

---
+++ TempReportApp/Report1VM.cs
@@ -0,0 +1,34 @@
+﻿using eOnlineCarShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TempReportApp
+{
+    public class Report1VM
+    {
+        public string Brand { get; set; }
+        public string CarModel { get; set; }
+        public string Fuel { get; set; }
+        public string VehicleType { get; set; }
+        public string Color { get; set; }
+        public string DriveType { get; set; }
+        public string Transmission { get; set; }
+        public int NumberOfSeats { get; set; }
+        public int NumberOfDors { get; set; }
+        public string NumberOfGears { get; set; }
+        public int PowerPS { get; set; }
+        public int PowerKw { get; set; }
+        public float WheelSize { get; set; }
+        public float Ccm { get; set; }
+        public float Kilometre { get; set; }
+        public DateTime DateOfManufacture { get; set; }
+        public DateTime DateOfFinish { get; set; }
+
+        public static List<Report1VM> Get()
+        {
+            return new List<Report1VM> { };
+        }
+    }
+}
---
+++ eOnlineCarShop/Controllers/Report1Controller.cs
@@ -0,0 +1,59 @@
+﻿using AspNetCore.Reporting;
+using Data_CS.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TempReportApp;
+
+namespace eOnlineCarShop.Controllers
+{
+    public class Report1Controller : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public Report1Controller(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public static Report1VM getCar(ApplicationDbContext db, int CarId)
+        {
+            Report1VM podaci = db.Car
+                .Where(w=> w.ID == CarId)
+                .Select(s => new Report1VM
+            {
+                Brand = s.brand.BrandName,
+                CarModel = s.Model,
+                NumberOfSeats = s.NumberOfSeats,
+                NumberOfDors = s.NumberOfDors,
+                NumberOfGears = s.NumberOfGears,
+                PowerKw = s.PowerKw,
+                PowerPS = s.PowerPS,
+                Ccm = s.Ccm,
+                WheelSize = s.WheelSize,
+                Kilometre = s.Kilometre,
+                DateOfManufacture = s.DateOfManufacture,
+                Fuel = s.Fuel.FuelName,
+                VehicleType = s.VehicleType.TypeName,
+                Color = s.Color.ColorName,
+                DriveType = s.DriveType.DriveTypeName,
+                Transmission = s.Transmission.TransmissionType
+            }).FirstOrDefault();
+
+            return podaci;
+        }
+        public IActionResult Index(int CarId)
+        {
+            AspNetCore.Reporting.LocalReport _localReport = new AspNetCore.Reporting.LocalReport("Reports/Report1.rdlc");
+            Report1VM podaci = getCar(_db, CarId);
+            _localReport.AddDataSource("DataSet1", podaci);
+
+            ReportResult result = _localReport.Execute(RenderType.Pdf);
+            return File(result.MainStream, "application/pdf");
+        }
+    }
+}
