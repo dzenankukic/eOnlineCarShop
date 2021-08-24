55438a0b9fb3eca8e9c41ceb212e2d1acd33055d
dzenan <dzenan.kuk@gmail.com>
2021-08-24 18:19:01 +0200

    SMS funkcionalnosti (nexmo) slanje SMS poruka administratorima sistema

---
+++ eOnlineCarShop/Controllers/SMSController.cs
@@ -0,0 +1,35 @@
+﻿using eOnlineCarShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Nexmo.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eOnlineCarShop.Controllers
+{
+    public class SMSController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(SMSMessageVM message)
+        {
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = "Your mobile shop" /*Configuration.Instance.Settings["appsettings:NEXMO_FROM_NUMBER"]*/,
+                to = message.To,
+                text = message.ContentMsg
+            });
+            return View();
+        }
+    }
+}
---
+++ eOnlineCarShop/ViewModels/SMSMessageVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eOnlineCarShop.ViewModels
+{
+    public class SMSMessageVM
+    {
+        [Display(Name = "Admin kontakt")]
+        public string To { get; set; }
+        [Display(Name = "Text poruke")]
+        public string ContentMsg { get; set; }
+    }
+}
